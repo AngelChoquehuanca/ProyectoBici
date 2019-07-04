5c98e66bec96feeb5e15edecf86aed6419fc5add
angel <aychp3000@gmail.com>
2019-07-03 21:53:21 -0500

    Agregando proyecto

---
+++ app/src/main/java/com/example/proyectobici/MapFragment.java
@@ -0,0 +1,96 @@
+package com.example.proyectobici;
+
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MapFragment extends Fragment implements OnMapReadyCallback {
+//public class MapFragment extends Fragment{
+
+    private GoogleMap mMap;
+    float miLat;
+    float miLong;
+
+    public MapFragment(float latitud, float longitud){
+        miLat=latitud;
+        miLong=longitud;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView =  inflater.inflate(R.layout.fragment_map, null, false);
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        /*mapFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+                googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
+                googleMap.clear(); //clear old markers
+
+                CameraPosition googlePlex = CameraPosition.builder()
+                        .target(new LatLng(37.4219999,-122.0862462))
+                        .zoom(10)
+                        .bearing(0)
+                        .tilt(45)
+                        .build();
+
+                googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(googlePlex), 10000, null);
+
+                mMap.addMarker(new MarkerOptions()
+                        .position(new LatLng(37.4219999, -122.0862462))
+                        .title("Spider Man")
+                        .icon(bitmapDescriptorFromVector(getActivity(),R.drawable.spider)));
+
+                mMap.addMarker(new MarkerOptions()
+                        .position(new LatLng(37.4629101,-122.2449094))
+                        .title("Iron Man")
+                        .snippet("His Talent : Plenty of money"));
+
+                mMap.addMarker(new MarkerOptions()
+                        .position(new LatLng(37.3092293,-122.1136845))
+                        .title("Captain America"));
+            }
+        });*/
+
+        return rootView;
+    }
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        //LatLng sydney = new LatLng(-34, 151);
+        LatLng sydney = new LatLng(miLat, miLong);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney,19));
+    }
+    /*public void updateMap(float latitud, float longitud){
+        //mMap.clear();
+        LatLng miPos = new LatLng(miLat, miLong);
+        mMap.addMarker(new MarkerOptions().position(miPos).title("Yo"));
+    }*/
+}
--- app/src/main/java/com/example/proyectobici/RouteActivity.java
+++ app/src/main/java/com/example/proyectobici/RouteActivity.java
@@ -1,19 +1,44 @@
 package com.example.proyectobici;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.app.Service;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.location.LocationProvider;
 import android.os.Bundle;
+import android.location.Geocoder;
+import java.util.Locale;
+import android.location.Address;
+import java.util.List;
 
+import com.google.android.gms.maps.GoogleMap;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Handler;
+import android.os.Message;
+import android.provider.Settings;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -22,7 +47,12 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.UUID;
 
-public class RouteActivity extends AppCompatActivity {
+public class RouteActivity extends AppCompatActivity implements LocationListener{
+    private FrameLayout frmMap;
+    private MapFragment mapFragment;
+    private FragmentManager fm;
+    private MapFragment fragment;
+
     private TextView mTextMessage;
     private TextView txtPulso;
     private TextView txtBT;
@@ -36,6 +66,13 @@ public class RouteActivity extends AppCompatActivity {
 
     private static final UUID BTMODULEUUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
     private static String address=null;
+
+    LocationManager locationManager;
+    private TextView txtLatitud;
+    private TextView txtLongitud;
+    private Location location;
+    private final int REQUEST_LOCATION = 200;
+    private static final String TAG = "RouteActivity";
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -59,6 +96,7 @@ public class RouteActivity extends AppCompatActivity {
             return false;
         }
     };
+
     @Override
     protected void onResume() {
         super.onResume();
@@ -102,10 +140,52 @@ public class RouteActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_route);
+
+
+        /*mapFragment=new MapFragment();
+        FragmentTransaction fragmentTransaction=getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.cmpFrameLayoutMap,mapFragment);
+        fragmentTransaction.commit();*/
+
+        fm=getSupportFragmentManager();
+        fragment=(MapFragment) fm.findFragmentById(R.id.cmpFrameLayoutMap);
+        if (fragment == null) {
+            fragment = new MapFragment((float)-16.40499, (float)-71.50177);
+            fm.beginTransaction()
+                    .add(R.id.cmpFrameLayoutMap, fragment)
+                    .commit();
+        }
+
+
         BottomNavigationView navView = findViewById(R.id.nav_view);
         mTextMessage = findViewById(R.id.message);
         txtPulso=(TextView)findViewById(R.id.cmpTxtPulso);
         txtBT=(TextView)findViewById(R.id.cmpTxtBT);
+        txtLatitud=(TextView)findViewById(R.id.cmpTxtLatitud);
+        txtLongitud=(TextView)findViewById(R.id.cmpTxtLongitud);
+
+
+
+
+        locationManager = (LocationManager) getSystemService(Service.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(RouteActivity.this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION);
+        } else {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 100, 2, this);
+            if (locationManager != null) {
+                location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+            }
+        }
+        if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            if (location != null) {
+                txtLatitud.setText(String.valueOf(location.getLatitude()));
+                txtLongitud.setText(String.valueOf(location.getLongitude()));
+                getAddressFromLocation(location, getApplicationContext(), new GeoCoderHandler());
+            }
+        } else {
+            showGPSDisabledAlertToUser();
+        }
+
         navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
         Intent intent=getIntent();
@@ -165,6 +245,111 @@ public class RouteActivity extends AppCompatActivity {
                 Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
                 startActivityForResult(enableBtIntent, 1);
             }
+        }
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        txtLatitud.setText(String.valueOf(location.getLatitude()));
+        txtLongitud.setText(String.valueOf(location.getLongitude()));
+        getAddressFromLocation(location, getApplicationContext(), new GeoCoderHandler());
+        /*fragment = new MapFragment((float)location.getLatitude(), (float)location.getLongitude());
+        fm.beginTransaction()
+                .add(R.id.cmpFrameLayoutMap, fragment)
+                .commit();*/
+        fragment = new MapFragment((float)location.getLatitude(), (float)location.getLongitude());
+        fm.beginTransaction()
+                .add(R.id.cmpFrameLayoutMap, fragment)
+                .commit();
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+        if (s.equals(LocationManager.GPS_PROVIDER)) {
+            showGPSDisabledAlertToUser();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_LOCATION) {
+            if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            }
+        }
+    }
+
+    private void showGPSDisabledAlertToUser() {
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        alertDialogBuilder.setMessage("GPS is disabled in your device. Would you like to enable it?")
+                .setCancelable(false)
+                .setPositiveButton("Goto Settings Page To Enable GPS", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        Intent callGPSSettingIntent = new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                        startActivity(callGPSSettingIntent);
+                    }
+                });
+        alertDialogBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                dialog.cancel();
+            }
+        });
+        AlertDialog alert = alertDialogBuilder.create();
+        alert.show();
+    }
+    public static void getAddressFromLocation(final Location location, final Context context, final Handler handler) {
+        Thread thread = new Thread() {
+            @Override public void run() {
+                Geocoder geocoder = new Geocoder(context, Locale.getDefault());
+                String result = null;
+                try {
+                    List<Address> list = geocoder.getFromLocation(
+                            location.getLatitude(), location.getLongitude(), 1);
+                    if (list != null && list.size() > 0) {
+                        Address address = list.get(0);
+                        // sending back first address line and locality
+                        result = address.getAddressLine(0) + ", " + address.getLocality() + ", " +  address.getCountryName() ;
+                    }
+                } catch (IOException e) {
+                    Log.e(TAG, "Impossible to connect to Geocoder", e);
+                } finally {
+                    Message msg = Message.obtain();
+                    msg.setTarget(handler);
+                    if (result != null) {
+                        msg.what = 1;
+                        Bundle bundle = new Bundle();
+                        bundle.putString("address", result);
+                        msg.setData(bundle);
+                    } else
+                        msg.what = 0;
+                    msg.sendToTarget();
+                }
+            }
+        };
+        thread.start();
+    }
+    private class GeoCoderHandler extends Handler {
+        @Override
+        public void handleMessage(Message message) {
+            String result;
+            switch (message.what) {
+                case 1:
+                    Bundle bundle = message.getData();
+                    result = bundle.getString("address");
+                    break;
+                default:
+                    result = null;
+            }
+            //currentCity.setText(result);
         }
     }
 
